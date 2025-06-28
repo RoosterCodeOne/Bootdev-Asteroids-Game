6bf9150873442386261c16b91f5bce74aeb177a5
RoosterCodeOne <alexlevy.ca@gmail.com>
2025-06-27 23:03:54 -0700

    Add circleshape class and player subclass. Adjusted fps

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    my_player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
